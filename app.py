204b69f11ff27287d37d45ce7933792c460ad1e1
Arnout Boelens <ampboelens@gmail.com>
2021-03-13 16:45:49 -0800

    disabled debug mode

--- app.py
+++ app.py
@@ -274,5 +274,5 @@ def update_map(zoomed, region, case, nlines, click, hover):
 
 
 if __name__ == '__main__':
-#    app.run_server()
-    app.run_server(debug=True)
+    app.run_server()
+#    app.run_server(debug=True)
